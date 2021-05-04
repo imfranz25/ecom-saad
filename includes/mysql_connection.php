8e8a40a3a5aec9bfd81432f8829a249da35bec23
Francis Ong <francis.ong25@gmail.com>
2021-05-04 21:58:42 +1200

    Apply Function (mysq_connect) for mysql connection

--- includes/mysql_connection.php
+++ includes/mysql_connection.php
@@ -3,19 +3,14 @@
 
 <?php
 
+    function mysql_connect(){
+    	//Initialization
+	    $dbhost = 'localhost';
+	    $dbuser = 'root';
+	    $dbpass = '';
+	    $dbname = 'ecommerce';
 
-function mysql_connect(){
-
-
-    //Initialization
-    $dbhost = 'localhost';
-    $dbuser = 'root';
-    $dbpass = '';
-    $dbname = 'ecommerce';
-
-    //mysql connection
-    return mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
-}
-
+	    return  mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+    }
 
 ?>
