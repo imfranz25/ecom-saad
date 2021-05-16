056d9d0dad72f0288fada5ead9ee1b6fd7055900
Francis Ong <francis.ong25@gmail.com>
2021-05-16 21:58:21 +1200

    Add onclick function (call product details js function)

--- functions/function.php
+++ functions/function.php
@@ -19,7 +19,7 @@
                 $id = $row[$table_name.'_id'];
                 $title = $row[$table_name.'_title'];
                 echo "<a href = 'index.php?product_".$table_name."=$id'>
-                        <li id = ".$table_name.$id.">$title</li></a>";
+                <li id = ".$table_name.$id.">$title</li></a>";
             }
         }
     }// END
@@ -32,7 +32,44 @@
                 $query = "SELECT * FROM product ORDER BY RAND() LIMIT 0,6";
                 $result = mysqli_query(mysql_connect(),$query);
 
-                while ($row = mysqli_fetch_array($result)) {
+                while ($row = mysqli_fetch_assoc($result)) {
+
+                    //get data
+                    $product_id = $row['product_id'];
+                    $product_cat = $row['product_category'];
+                    $product_brand = $row['product_brand'];
+                    $product_title = $row['product_title'];
+                    $product_price = $row['product_price'];
+                    $product_image = $row['product_image'];
+
+                    echo "<script> var a = $product_price;</script>
+                            <div id = 'single_product'>
+                            <center>
+                            <h3> $product_title </h3>
+                            <img src = 'admin_area/product_images/$product_image'>
+                            <p><b>Price : PHP $product_price </b></p>
+                            <a onclick = 'product_details(".json_encode($product_title).");'>Details</a>
+                            <a href = 'index.php'><button>Add to Cart</button></a>
+                            </center>
+                            </div>" ;
+                }
+            }
+        }
+    }// END
+
+    function get_sort_product($product){
+        if (isset($_GET['product_'.$product])) {
+            
+            $id = $_GET['product_'.$product];
+            $query = "SELECT * FROM product WHERE product_".$product." = '$id' ";
+            $result = mysqli_query(mysql_connect(),$query);
+            $count = mysqli_num_rows($result);
+
+            if ($count == 0) {
+                echo "<h2 style = 'padding:50px 30px 50px 30px;background-color:rgb(23,183,183,.9);text-shadow:3px 2px black;border-radius:5px;margin-top:5%;'>There is no product available from this $product !</h2>";
+            }
+
+            while ($row = mysqli_fetch_array($result)) {
 
                     //get data
                     $product_id = $row['product_id'];
@@ -47,52 +84,18 @@
                             <h3> $product_title </h3>
                             <img src = 'admin_area/product_images/$product_image'>
                             <p><b>Price : PHP $product_price </b></p>
-                            <a href = 'details.php'>Details</a>
+                            <a href = 'index.php' onclick = 'alert('hhaha')'>Details</a>
                             <a href = 'index.php'><button>Add to Cart</button></a>
                             </center>
                             </div>" ;
-                }
-            }
-        }
-    }// END
-
-    function get_sort_product($product){
-
-        if(isset($_GET['product_'.$product])){ 
-            //get cat id
-            $id = $_GET['product_'.$product];
-            $query = "SELECT * FROM product WHERE product_".$product." = '$id' ";
-            $result = mysqli_query(mysql_connect(), $query);
-            $count = mysqli_num_rows($result);
-
-            if($count == 0){
-                echo "<h2 style='padding:50px 20px 50px 20px;background-color:rgb(23,183,183,.9);text-shadow:3px 2px black;border-radius:5px;margin-top:5%;'>There is no available product in this $product !</h2>";
             }
 
-            while($row = mysqli_fetch_array($result)){
+            echo "<script>document.getElementById('$product' + $id).style = 'background-color:rgb(23,183,183,.9);'</script>";
 
-                //fetch values
-                $product_id = $row['product_id'];
-                $product_category = $row['product_category'];
-                $product_brand = $row['product_brand'];
-                $product_title = $row['product_title'];
-                $product_price = $row['product_price'];
-                $product_image = $row['product_image'];
-
-                echo "<div id = 'single_product'>
-                        <center>
-                        <h3> $product_title </h3>
-                        <img src = 'admin_area/product_images/$product_image'>
-                        <p><b>Price : PHP $product_price </b></p>
-                        <a href = 'details.php'>Details</a>
-                        <a href = 'index.php'><button>Add to Cart</button></a>
-                        </center>
-                        </div>" ;
-            }
-
-            echo "<script>document.getElementById('$product'+$id).style = 'background-color: rgb(23,183,183,.9)'</script>";
         }
     }
+
+   
 
 
     
